5c39d7334962c57400f5ae161e137f59e55c564c
Flores Román <jorge-antonio.flores-roman@capgemini.com>
2019-03-11 13:33:45 +0100

    comentario del commit

---
+++ src/main/java/com/proyecto/PrimerProyectoApplication.java
@@ -0,0 +1,29 @@
+//Aplicación principal
+
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.proyecto.conf.SwaggerConfiguration;
+
+@SpringBootApplication
+@Import(SwaggerConfiguration.class)
+public class PrimerProyectoApplication implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoApplication.class, args);
+	}
+	
+	   @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	 
+	           registry.addResourceHandler("swagger-ui.html")
+	                    .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    }
+
+}
